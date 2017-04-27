fe2a3b6895d947de1ab526afb3aa3296763a59d4
Vinicius Teixeira <vinicius0026@gmail.com>
2017-04-27 19:45:49 -0300

    fixing margin and code indentation

--- src/components/modules/login/Login.vue
+++ src/components/modules/login/Login.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="content has-text-centered">
-  <h1 class="is-title is-bold">Login</h1>
+    <h1 class="is-title is-bold">Login</h1>
 
-  <div class="columns is-vcentered">
-    <div class="column is-6 is-offset-3">
-      <div class="box">
-        <form v-on:submit.prevent="doLogin">
-          <label class="label">Username</label>
-          <p class="control">
-            <input v-model="username" class="input" type="text" placeholder="username">
-          </p>
-          <label class="label">Password</label>
-          <p class="control">
-            <input v-model="password" class="input" type="password" placeholder="password">
-          </p>
-          <br>
-          <p class="control">
-            <button type="submit" class="button is-primary">Login</button>
-            <button class="button is-default">Cancel</button>
-          </p>
-        </form>
+    <div class="columns is-vcentered">
+      <div class="column is-6 is-offset-3">
+        <div class="box">
+          <form v-on:submit.prevent="doLogin">
+            <label class="label">Username</label>
+            <p class="control">
+              <input v-model="username" class="input" type="text" placeholder="username">
+            </p>
+            <label class="label">Password</label>
+            <p class="control">
+              <input v-model="password" class="input" type="password" placeholder="password">
+            </p>
+            <br>
+            <p class="control">
+              <button type="submit" class="button is-primary">Login</button>
+              <button class="button is-default">Cancel</button>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -50,4 +50,7 @@ export default {
 </script>
 
 <style scoped>
+.content {
+  margin-top: 100px;
+}
 </style>
