ab1da89a60701fc8f411e7d6215f57727e898b59
Vinicius Teixeira <vinicius0026@gmail.com>
2017-04-27 22:51:20 -0300

    loading users data and displaying in users screen

--- src/components/modules/users/UsersList.vue
+++ src/components/modules/users/UsersList.vue
@@ -1,12 +1,43 @@
 <template>
-  <div>
-    users
+  <div class="tile is-ancestor">
+    <div class="tile is-parent">
+      <article class="tile is-child box">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>username</th>
+              <th>roles</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="user in users">
+              <td>{{ user.id }}</td>
+              <td>{{ user.username }}</td>
+              <td>{{ user.scope }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </article>
+    </div>
   </div>
 </template>
 
 <script>
+import usersService from './usersService'
 export default {
-  name: 'users'
+  name: 'users',
+  data () {
+    return {
+      users: []
+    }
+  },
+  created () {
+    usersService.list()
+      .then(res => {
+        this.users = res.data
+      })
+  }
 }
 </script>
 
