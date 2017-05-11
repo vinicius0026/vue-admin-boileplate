97d1c1668ed88d139f0b025e2a853f0fd37accae
Vinicius Teixeira <vinicius0026@gmail.com>
2017-05-10 22:21:58 -0300

    adding interceptor for 401 responses

--- src/components/modules/login/loginService.js
+++ src/components/modules/login/loginService.js
@@ -1,7 +1,17 @@
 'use strict'
 
 import axios from 'axios'
+import router from '../../../router'
+import store from '../../../store'
 import { auth } from '../../../constants/api'
+
+axios.interceptors.response.use(res => res, err => {
+  if (err.response.status === 401 && store.getters.isAuthenticated) {
+    store.dispatch('logout')
+    router.push({ name: 'login' })
+  }
+  return Promise.reject(err)
+})
 
 export default {
   login,
