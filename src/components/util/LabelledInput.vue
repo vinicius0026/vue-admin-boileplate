d075e097a4d3dcec3016f5091421853241d2594d
Vinicius Teixeira <vinicius0026@gmail.com>
2017-05-11 19:59:29 -0300

    created LabelledInput component

---
+++ src/components/util/LabelledInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="field">
+    <label class="label">{{ label }}</label>
+    <p class="control">
+      <input class="input"
+             :type="type"
+             :placeholder="placeholder"
+             ref="input"
+             v-bind:value="value"
+             v-on:input="updateValue($event.target.value)">
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LabelledInput',
+  props: {
+    value: {},
+    label: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    placeholder: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    updateValue: function (value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
